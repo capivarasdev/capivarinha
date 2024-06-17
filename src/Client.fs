6af5321221ba3c5e7a18a785d129def88413f565
Victor Freire <victor@freire.dev.br>
2024-06-17 15:48:23 -0300

    change format

--- src/Client.fs
+++ src/Client.fs
@@ -59,7 +59,7 @@ module Action =
         | value, CanRoll ->
             do! missedRoll reactionUser value
         | _, CannotRoll timeLeft ->
-            let diffStr = timeLeft.ToString(@"%dd' days'\ mm' mins'\ ss' secs'")
+            let diffStr = timeLeft.ToString(@"%d' days'\ hh' hours'\ mm' mins'")
             let! _ = textChannel.SendMessageAsync(sprintf "%s needs to wait %s before rolling a die again!" reactionUser.Mention diffStr)
             let! _ = message.RemoveReactionAsync(reaction.Emote, reactionUser)
             ()
